5e582197652fcc3b2715f63793cad85f1c57ce8d
Karan Singh <karan@Karans-MacBook-Air.local>
2025-03-12 16:25:34 +0530

    scope page added

---
+++ src/Components/Scope/Scope.jsx
@@ -0,0 +1,18 @@
+
+function Scope() {
+  return (
+    <>
+    <div>
+      {/* Shaily */}
+    </div>
+    <div>
+    {/* Ankur */}
+  </div>
+  <div>
+  {/* Karan */}
+</div>
+</>
+  )
+}
+
+export default Scope
