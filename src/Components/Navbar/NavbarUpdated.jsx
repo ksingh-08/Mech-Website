b65df0ef725f32d627e03eda0cc4c8758514141c
Karan Singh <karan@Karans-MacBook-Air.local>
2025-03-15 16:11:43 +0530

    responsive issue

--- src/Components/Navbar/NavbarUpdated.jsx
+++ src/Components/Navbar/NavbarUpdated.jsx
@@ -31,7 +31,7 @@ const NavbarUpdated = () => {
 
   const [scrolling, setScrolling] = useState(false);
   return (
-    <nav  className={`z-10 flex justify-between items-center w-[89%] bg-red-900 fixed left-1/2 transform -translate-x-1/2  p-6  h-20 shadow-xl
+    <nav  className={` z-10 flex justify-between items-center w-[89%] bg-red-900 fixed left-1/2 transform -translate-x-1/2  p-6  h-20 shadow-xl
     ${scrolling ? " top-0 w-full rounded-none transition-all" : ' top-5 rounded-xl transition-all'}`}>
       <img src={logo} alt="" className='w-40 ' />
 
@@ -57,14 +57,15 @@ const NavbarUpdated = () => {
   className="md:hidden text-white text-3xl focus:outline-none transition-transform duration-300" 
   onClick={menuHandler}
 >
-  <span className={`transition-transform duration-1000 ${openMenu ? 'rotate-180' : 'rotate-0'}`}>
+  <span className={`transition-transform duration-1000 ${openMenu ? 'rotate-180' : 'rotate-0'} `}>
     {openMenu ? <GrClose /> : <FiMenu />}
   </span>
 </button>
 
 {/* Mobile Dropdown Menu */}
+
 <ul 
-  className={`${scrolling ? "w-full rounded-none transition-all" : 'rounded-xl transition-all'} absolute  top-20 left-0 w-full bg-gray-900 text-white flex flex-col items-center gap-6 py-4 shadow-lg md:hidden transition-all duration-300 ease-in-out ${openMenu ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`}
+  className={` ${scrolling ? "w-full rounded-none transition-all" : 'rounded-xl transition-all'} absolute  top-20 left-0 w-full bg-gray-900 text-white flex flex-col items-center gap-6 py-4 shadow-lg md:hidden transition-all duration-300 ease-in-out ${openMenu ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}`}
 >
   <li className='hover:scale-105'>
     <a href="/" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full">Home</a>
