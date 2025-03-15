5384ab700cf4527983b2b12d5772b7aa9bab3285
Karan Singh <karan@Karans-MacBook-Air.local>
2025-03-15 15:26:06 +0530

    navbar responsive

--- src/Components/Navbar/NavbarUpdated.jsx
+++ src/Components/Navbar/NavbarUpdated.jsx
@@ -35,25 +35,62 @@ const NavbarUpdated = () => {
     ${scrolling ? " top-0 w-full rounded-none transition-all" : ' top-5 rounded-xl transition-all'}`}>
       <img src={logo} alt="" className='w-40 ' />
 
-      <ul className={`flex gap-12 [&>li>a]:cursor-pointer [&>li>a]:text-white justify-center `}>
-      <li className='hover:scale-105'><a href="/" className='hover:text-orange-100 '>Home</a></li>
-      <li className='hover:scale-105'><NavLink className='hover:text-orange-100' to="/admission">Admission</NavLink></li>
+      <ul className={`hidden md:flex gap-12 [&>li>a]:cursor-pointer [&>li>a]:text-white justify-center `}>
+      <li className='hover:scale-105'><a href="/" className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full">Home</a></li>
+      <li className="hover:scale-105"><NavLink className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/scope">Why LNMIIT?</NavLink></li>
+      {/* <li className='hover:scale-105'><NavLink className=" text-xl relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/admission">Admission</NavLink></li> */}
       {/* <li><NavLink to="/programs">Projects</NavLink></li> */}
-      <li className='hover:scale-105'><NavLink className='hover:text-orange-100' to="/about">About</NavLink></li>
-      <li className='hover:scale-105'><NavLink className='hover:text-orange-100' to="/career">Careers</NavLink></li>
-      <li className='hover:scale-105'><NavLink className='hover:text-orange-100' to="/facility">Facility</NavLink></li>
-      <li className='hover:scale-105'><NavLink className='hover:text-orange-100' to="/research">Research</NavLink></li> 
-      <li className="hover:scale-105"><NavLink className="hover:text-orange-100" to="/scope">Why LNMIIT?</NavLink></li>
+      <li className='hover:scale-105'><NavLink className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/about">About</NavLink></li>
+      
+      <li className='hover:scale-105'><NavLink className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/career">Careers</NavLink></li>
+      <li className='hover:scale-105'><NavLink className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/facility">Facility</NavLink></li>
+      <li className='hover:scale-105'><NavLink className=" text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/research">Research</NavLink></li> 
+      
       </ul>
 
       <div>
-        <button className='btn'>
+        <button className='btn hidden md:block'>
         <NavLink to="/contact">Contact</NavLink>
         </button> 
       </div>
+      <button 
+  className="md:hidden text-white text-3xl focus:outline-none transition-transform duration-300" 
+  onClick={menuHandler}
+>
+  <span className={`transition-transform duration-1000 ${openMenu ? 'rotate-180' : 'rotate-0'}`}>
+    {openMenu ? <GrClose /> : <FiMenu />}
+  </span>
+</button>
+
+{/* Mobile Dropdown Menu */}
+<ul 
+  className={`absolute top-20 left-0 w-full bg-gray-900 text-white flex flex-col items-center gap-6 py-4 shadow-lg md:hidden transition-all duration-300 ease-in-out ${openMenu ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'}${scrolling ? " top-0 w-full rounded-none transition-all" : ' top-5 rounded-xl transition-all'}`}
+>
+  <li className='hover:scale-105'>
+    <a href="/" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full">Home</a>
+  </li>
+  <li className="hover:scale-105">
+    <a href="/scope" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/">Why LNMIIT?</a>
+  </li>
+  <li className='hover:scale-105'>
+    <a href="/about" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/about">About</a>
+  </li>
+  <li className='hover:scale-105'>
+    <a href="/career" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/career">Careers</a>
+  </li>
+  <li className='hover:scale-105'>
+    <a href="/facility" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/facility">Facility</a>
+  </li>
+  <li className='hover:scale-105'>
+    <a href="/research" className="text-xl font-medium relative transition-all duration-300 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-orange-100 after:transition-all after:duration-300 hover:after:w-full" to="/research">Research</a>
+  </li> 
+</ul>
+
+          
       
     </nav>
   );
 };
 
 export default NavbarUpdated;
+
