c5b128f8ec592a91a2b15db2fdd4be03619d166b
Shaily Chaturvedi <shaily17102003@gmail.com>
2025-04-01 12:45:45 +0530

    added facility preface and carousel modifications

--- src/Components/Facility/Facility.jsx
+++ src/Components/Facility/Facility.jsx
@@ -10,64 +10,62 @@ import fl7 from '../../assets/fl7.png'
 import fl8 from '../../assets/fl8.jpg'
 import Title from '../Title/Title'
 import { Link } from 'react-router-dom'
- 
 
 const Facility = () => {
   return (
     <>
-    <Title   title='LAB FACILITIES'/>
-    <div className='outerFacility'>
-     <div className="Facilitycontainer">
-      <Link to="/facility1">  <a href="/facility1" className="cardFacility">
-      <h2>SMAW</h2>
-      <img src={fl1} alt=""/>
-     </a></Link>
-   
-    
-    <a href=" /facility2" className="cardFacility">
-      <h2>SAW</h2>
-      <img src={fl2} alt="" />
-     </a>
+      <Title title='LAB FACILITIES' />
 
-    <a href="/facility3" className="cardFacility">
-      <h2>GMAW or MIG</h2>
-      <img src={fl3} alt="" />
-     </a>
+      {/* Preface Section */}
 
-    <a href="/facility4" className="cardFacility">
-      <h2>GTAW</h2>
-      <img src={fl4} alt="" />
-     </a>
+      <div className='outerFacility'>
+        <div className="Facilitycontainer">
+          <Link to="/facility1">  <a href="/facility1" className="cardFacility">
+            <h2>SMAW</h2>
+            <img src={fl1} alt=""/>
+          </a></Link>
 
-    <a href="/facility5" className="cardFacility">
-      <h2>Oxyfuel Welding</h2>
-      <img src={fl5} alt="" />
-     </a>
+          <a href=" /facility2" className="cardFacility">
+            <h2>SAW</h2>
+            <img src={fl2} alt="" />
+          </a>
 
-    <a href="/facility6" className="cardFacility">
-      <h2>Robotic Welding</h2>
-      <img src={fl6} alt="" />
-     </a>
-      
-      <Link to="/facility7"> 
-    <a href=" " className="cardFacility">
-      <h2>Plasma Cutting</h2>
-      <img src={fl7} alt="" />
-     </a>
-     </Link>
+          <a href="/facility3" className="cardFacility">
+            <h2>GMAW or MIG</h2>
+            <img src={fl3} alt="" />
+          </a>
 
-     <Link to="/facility8"> 
-    <a href=" " className="cardFacility">
-      <h2>Consumable Development</h2>
-      <img src={fl8} alt="" />
-     </a>
-     </Link>
-     
-    
-  </div>    
-    
-      
-    </div>
+          <a href="/facility4" className="cardFacility">
+            <h2>GTAW</h2>
+            <img src={fl4} alt="" />
+          </a>
+
+          <a href="/facility5" className="cardFacility">
+            <h2>Oxyfuel Welding</h2>
+            <img src={fl5} alt="" />
+          </a>
+
+          <a href="/facility6" className="cardFacility">
+            <h2>Robotic Welding</h2>
+            <img src={fl6} alt="" />
+          </a>
+
+          <Link to="/facility7"> 
+            <a href=" " className="cardFacility">
+              <h2>Plasma Cutting</h2>
+              <img src={fl7} alt="" />
+            </a>
+          </Link>
+
+          <Link to="/facility8"> 
+            <a href=" " className="cardFacility">
+              <h2>Consumable Development</h2>
+              <img src={fl8} alt="" />
+            </a>
+          </Link>
+
+        </div>
+      </div>
     </>
   )
 }
--- src/Components/Facility/FacilityUpated.jsx
+++ src/Components/Facility/FacilityUpated.jsx
@@ -4,10 +4,10 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import './fac.css'
-import Title from '../Title/Title'
+import './fac.css';
+import Title from '../Title/Title';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import slide_image1 from '../../newpics/SMAW.jpeg';
 import slide_image2 from '../../newpics/SAW.jpeg';
 import slide_image3 from '../../newpics/GMAW.jpeg';
@@ -16,12 +16,11 @@ import slide_image5 from '../../assets/fl5.jpg';
 import slide_image6 from '../../newpics/Robotic.jpeg';
 import slide_image7 from '../../newpics/Plasma.jpeg';
 import slide_image8 from '../../assets/fl8.jpg';
-import smaw from '../../newpics/SMAW.jpeg'
 import Particles from '../../UI/Particles';
 
 const FacilityUpdated = () => {
   return (
-    <div className="facility-container relative w-full h-[600px]">
+    <div className="facility-container relative w-full min-h-screen pb-20">
       {/* Particles as Background */}
       <div className="absolute inset-0 -z-10">
         <Particles
@@ -36,13 +35,27 @@ const FacilityUpdated = () => {
         />
       </div>
 
-      {/* Content */}
-      <Title title="LAB FACILITIES" className="title2" />
+      {/* Title */}
+      <Title title="LAB FACILITIES" className="title2 pt-10" />
+
+      {/* Preface Section */}
+      <div className="facility-preface px-6 py-6 bg-white shadow-md rounded-md mb-6 max-w-3xl mx-auto">
+        <p className="text-gray-700 text-lg leading-relaxed">
+          At <strong className='text-red-800 font-semibold'>LNMIIT</strong>, we are at the forefront of <strong className='text-red-800 font-semibold'>welding research and innovation</strong>, 
+          providing cutting-edge solutions for industries of all scales. Whether you're a <strong className='text-red-800 font-semibold'>large enterprise </strong> 
+          seeking advanced welding techniques or a <strong className='text-red-800 font-semibold'>growing business</strong> looking for expert consultancy, 
+          our state-of-the-art <strong className='text-red-800 font-semibold'>welding research lab</strong> is equipped to meet all your needs. <br /><br />
+          <h3><strong className='text-red-800 font-semibold'>Explore our world-class facilities and discover the future of welding technology.</strong></h3>
+        </p>
+      </div>
+
+      {/* Swiper Carousel */}
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={'auto'}
+        loop={true}
         coverflowEffect={{
           rotate: 40,
           stretch: 0,
@@ -57,26 +70,34 @@ const FacilityUpdated = () => {
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
-        className="mySwiper"
+        className="mySwiper "
       >
         <SwiperSlide className="swiper-slide bg-white shadow-xl rounded-xl p-2 border border-gray-200">
-  <Link to="/facility1">
-    <img src={slide_image1} alt="SMAW" className="rounded-md" />
-    <div className="title1 text-gray-800 font-semibold mt-2">
-      <span>SMAW</span>
+          <Link to="/facility1">
+            <img src={slide_image1} alt="SMAW" className="rounded-md" />
+            <div className="title1 text-gray-800 font-semibold mt-2">
+              <span>SMAW</span>
+            </div>
+          </Link>
+        </SwiperSlide>
+
+        <SwiperSlide key={Math.random()} className="swiper-slide bg-white shadow-md rounded-xl p-2">
+  <Link to="/facility2">
+    <img src={slide_image2} alt="SAW" />
+
+    {/* Centering Label */}
+    <div className="title1 flex justify-center mt-2">
+      {/* Single Border Only */}
+      <div className="border border-black text-black px-6 py-3 rounded-lg shadow-md 
+                      transition duration-300 hover:bg-red-800 hover:text-white cursor-pointer">
+        SAW
+      </div>
     </div>
+
   </Link>
 </SwiperSlide>
 
 
-        <SwiperSlide className="swiper-slide bg-white shadow-md rounded-xl p-2 border border-gray-200">
-          <Link to="/facility2">
-            <img src={slide_image2} alt="SAW" />
-            <div className="title1">
-              <span>SAW</span>
-            </div>
-          </Link>
-        </SwiperSlide>
 
         <SwiperSlide className="swiper-slide bg-white shadow-sm rounded-xl p-2 border border-gray-200">
           <Link to="/facility3">
@@ -150,106 +171,3 @@ const FacilityUpdated = () => {
 };
 
 export default FacilityUpdated;
-
-
-
-  // return (
-  //   <div className="facility-container">
-  //      <Title title='LAB FACILITIES' className='title2'/>
-  //     <Swiper
-  //       effect={'coverflow'}
-  //       grabCursor={true}
-  //       centeredSlides={true}
-  //       loop={true}
-  //       slidesPerView={'auto'}
-  //       initialSlide={2}
-  //       coverflowEffect={{
-  //         rotate: 0,
-  //         stretch: 0,
-  //         depth: 100,
-  //         modifier: 1,
-  //         slideShadows:true,
-          
-  //       }}
-  //       pagination={{ el: '.swiper-pagination', clickable: true }}
-        // navigation={{
-        //   nextEl: '.next',
-        //   prevEl: '.prev',
-        //   clickable: true,
-        // }}
-  //       modules={[EffectCoverflow, Pagination, Navigation]}
-  //       className="swiper_container"
-  //     >
-        
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility1"> 
-        //   <img src={slide_image1} alt="slide_image" />
-        //   <div className='title1'>
-        //     <span>SMAW</span></div>
-        //     </Link>
-        // </SwiperSlide>
-        
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility2">
-        //   <img src={slide_image2} alt="slide_image" />
-        //   <div className='title1'><span>SAW</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility3">
-        //   <img src={slide_image3} alt="slide_image" />
-        //   <div className='title1'><span>GMAW or MIG</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility4">
-        //   <img src={slide_image4} alt="slide_image" />
-        //   <div className='title1'><span>GTAW</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility5">
-        //   <img src={slide_image5} alt="slide_image" />
-        //   <div className='title1'><span>Oxyfuel Welding</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility6">
-        //   <img src={slide_image6} alt="slide_image" />
-        //   <div className='title1'><span>Robotic Welding</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility7">
-        //   <img src={slide_image7} alt="slide_image" />
-        //   <div className='title1'><span>Plasma Cutting</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        // <SwiperSlide className='swiper-slide'>
-        // <Link to="/facility8">
-        //   <img src={slide_image8} alt="slide_image" />
-        //   <div className='title1'><span>Consumable Development</span></div>
-        //   </Link>
-        // </SwiperSlide>
-        
-  //     </Swiper>
-      
-     
-      // <div className="slider-controler">
-      
-      //     <div className="slider-arrow prev">  {/* Updated to use 'prev' class */}
-      //       <ion-icon name="arrow-back-circle-outline" className="prev"></ion-icon>
-      //     </div>
-
-      //     <div className="slider-arrow next">  {/* Updated to use 'next' class */}
-      //       <ion-icon name="arrow-forward-circle-outline" className="next"></ion-icon>
-      //     </div>
-      //     <div className="swiper-pagination "></div>
-  
-      // </div>
-        
-      
-     
-      
-  //   </div>
-  // );
